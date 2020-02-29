d9fb0fa290fcbfb3a26f8c54e5f0f56fdb89b544
DoWhatThouWilt <qhorizon@gmail.com>
2020-02-29 14:46:30 -0500

    setting up the registration page

--- server/src/app.js
+++ server/src/app.js
@@ -7,12 +7,14 @@ const morgan = require('morgan')
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
+//app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
-app.get('/status', (req, res) => {
-    res.send({
-        message: 'hello world!!!!!!1'
-    })
-})
-
 app.listen(process.env.PORT || 8081)
+
+app.post('/register', (req, res) => {
+    res.send({
+        message: `Hello ${req.body.email}! Does this work?`
+    })
+    console.log(req.body)
+})
